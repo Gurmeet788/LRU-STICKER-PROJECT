da1cec5440fa23e341980cbb2a9650a9dcd3b35b
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-11-23 00:56:43 +0500

    Add get, put, and cache retrieval methods to LRUcache
    Implemented remove, get, put, and get_all_cache methods in the LRUcache class to support cache item retrieval, insertion, eviction, and inspection. These changes enhance the cache's functionality and usability.

--- backend/stickers/lru_cache.py
+++ backend/stickers/lru_cache.py
@@ -21,3 +21,47 @@ class LRUcache:
         self.head.next.prev = node
         self.head.next = node
         node.prev = self.head
+
+    def remove(self,node):
+        next_node = node.next
+        prev_node = node.prev
+        prev_node.next = next_node
+        next_node.prev = prev_node
+    
+    def get(self,key):
+
+        if key not in self.cache:
+            return None
+        
+        node = self.cache[key]
+        self.remove(node=node)
+        self.add_front(node=node)
+
+        return node.value
+    
+    def put(self, key, value):
+
+        if key in self.cache:
+
+            node = self.cache[key]
+            self.remove(node=node)
+            self.add_front(node=node)
+        else:
+
+            if len(self.cache) >= self.capacity:
+                lru = self.tail.prev
+                self.remove(node=lru)
+                del self.cache[lru.key]
+
+            newnode = Node(key=key, value= value)
+            self.cache[key] = newnode
+            self.add_front(newnode)
+    
+    def get_all_cache(self):
+        result = []
+        current = self.head.next
+        while current != self.tail:
+            result.append({current.key: current.value})
+            current = current.next
+        return result
+            
